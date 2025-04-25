4d2c03cf4001ccdb1715f3d1e4521179d4ec94ca
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-04-25 15:46:05 +0200

    feat: implemented card component in explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
 
 function Explore() {
   return (
     <>
     <Navbar />
+    <Card />
     <Footer />
     </>
   );
