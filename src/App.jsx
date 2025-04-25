11d0bbeffb68bfa508f8a5f054d9440d40d44e19
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-04-25 15:03:26 +0200

    feat: implemented hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
     <Navbar />
+    <HeroSection />
+    </>
   );
 }
 
