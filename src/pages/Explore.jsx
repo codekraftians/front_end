a655bf4c99b3c3b9673243bd859dadd305da7cdb
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-04-27 18:16:56 +0200

    feat: implement pagination in explore page, set number of events to 300 to see if pagination works

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,13 @@
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
+import Pagination from "../components/Pagination";
 
 function Explore() {
 
   // datos de ejemplo para los eventos, cambiar por datos reales cuando backend esté listo
-  const events = Array.from({ length: 15 }, (_, index) => ({
+  const events = Array.from({ length: 300 }, (_, index) => ({
     id: index,
     title: `Event ${index + 1}`,
     image: 'https://external-preview.redd.it/moqwXPEx6GDBtYvFl5I51lltcFwBC_SvFuEDiO2mRaM.jpg?width=640&crop=smart&auto=webp&s=b95063ca9510280511942f855088c7b7749760a5',
@@ -13,7 +15,17 @@ function Explore() {
     isNew: index % 2 === 0,
   }));
 
-  // Función para determinar si el evento es nuevo, añade la badge "NEW" si hace menos de 5 días que fue creado, listo para usar cuando el backend esté listo
+  //esto es la paginación, ya puesta con 15 eventos por página
+  const eventsPerPage = 15;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastEvent = currentPage * eventsPerPage;
+  const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+  const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
+  
+  const totalPages = Math.ceil(events.length / eventsPerPage);
+
+  // Función para determinar si el evento es nuevo, añade la badge "NEW" si hace menos de 5 días que fue creado, ready para usar cuando el backend esté listo
   const isNew = (createdAt) => {
     const createdDate = new Date(createdAt);
     const now = new Date();
@@ -24,17 +36,22 @@ function Explore() {
   return (
     <>
     <Navbar />
-    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
-      {events.map((event) => (
-        <Card 
-          key={event.id}
-          title={event.title}
-          image={event.image}
-          description={event.description}
-          isNew={event.isNew}
-        />
+      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center">
+        {currentEvents.map((event) => (
+          <Card 
+            key={event.id}
+            title={event.title}
+            image={event.image}
+            description={event.description}
+            isNew={event.isNew}
+          />
         ))}
       </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     <Footer />
     </>
   );
