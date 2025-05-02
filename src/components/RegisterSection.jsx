60273535273597bf0a0141510a1b0ad82eb4e234
einartech <imalovelyghost13@gmail.com>
2025-05-02 14:11:53 +0200

    feat(user register):create user base made

--- src/components/RegisterSection.jsx
+++ src/components/RegisterSection.jsx
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const RegisterSection = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e.target[0].value); // username
-    console.log(e.target[1].value); // email
-    console.log(e.target[2].value); // password
-    handleClear();
-  };
+  const [name, setName] = useState("admin");
+  const [email, setEmail] = useState("admin@gmail.com");
+  const [password, setPassword] = useState("admin123!");
+  const [user_image] = useState("https://picsum.photos/200");
 
-  const handleClear = () => {
-    document.querySelector('input[type="text"]').value = '';
-    document.querySelector('input[type="email"]').value = '';
-    document.querySelector('input[type="password"]').value = '';
-  }
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const user = { email, name, password, user_image };
+    console.table(user);
+
+    fetch("http://localhost:8080/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to create user");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("New user added:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-[70vh]">
       <div className="bg-white shadow-md p-8 rounded-md w-80 text-center">
         <h1 className="text-lg mb-6">Register</h1>
 
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="form-control mb-4">
             <input
               type="text"
               placeholder="Username"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="input input-bordered w-full"
               required
             />
@@ -34,10 +54,14 @@ const RegisterSection = () => {
             <input
               type="email"
               placeholder="mail@site.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="input input-bordered w-full"
               required
             />
-            <div className="validator-hint text-xs text-gray-500 text-left">Enter valid email address</div>
+            <div className="validator-hint text-xs text-gray-500 text-left">
+              Enter valid email address
+            </div>
           </div>
 
           <div className="form-control mb-2">
@@ -46,16 +70,18 @@ const RegisterSection = () => {
               className="input input-bordered w-full"
               required
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               minLength="8"
               pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
               title="Must be at least 8 characters, including a number, a lowercase letter, and an uppercase letter"
             />
-            
+
             <p className="validator-hint text-xs text-gray-500 mb-4 text-left">
               Must be more than 8 characters, including
-              <br/>• At least one number
-              <br/>• At least one lowercase letter
-              <br/>• At least one uppercase letter
+              <br />• At least one number
+              <br />• At least one lowercase letter
+              <br />• At least one uppercase letter
             </p>
           </div>
 
@@ -63,6 +89,7 @@ const RegisterSection = () => {
             <button
               type="submit"
               className="btn btn-sm bg-primary text-white border-none hover"
+              onClick={handleClick}
             >
               Create
             </button>
