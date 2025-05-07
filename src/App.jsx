dce1f690573adbd51221733de936b08a606ab507
saudlari <larissa@riseup.net>
2025-05-08 00:44:14 +0200

    styles(Hero): delete scroll in Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
-    <>
-    <Navbar />
-    <HeroSection />
-    <Footer />
-    </>
+    <div className="flex flex-col h-screen overflow-hidden">
+      <Navbar />
+      <main className="flex-grow overflow-auto">
+        <HeroSection />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 h-full">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">
